1a92e5d11752485a94f233452b4ef1214d651764
Carlos35654E <arriolacarlos64@gmail.com>
2026-04-25 15:27:38 -0600

    beta-2

--- js/app.js
+++ js/app.js
@@ -54,4 +54,15 @@ function cambiarColor() {
     });
 }
 
+// Lógica del menú hamburguesa
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav');
+
+if (hamburger && nav) {
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        nav.classList.toggle('active');
+    });
+}
+
 cambiarColor()
